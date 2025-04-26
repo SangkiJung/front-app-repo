25646252e1b4ee383b2f68f14a9eea36d920ccc1
EC2 Default User <ec2-user@ip-172-16-0-11.ap-northeast-2.compute.internal>
2025-04-26 00:04:42 +0000

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version 1
+            EKS DEMO Blog version 5
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
